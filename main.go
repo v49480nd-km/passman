102a30b696b450cf9347384d27d59d6acf25fbce
km <169460914+v49480nd-km@users.noreply.github.com>
2025-01-15 17:30:34 -0500

    learning git  a lil bit

--- main.go
+++ main.go
@@ -7,10 +7,23 @@ func start_msg_cli() {
     fmt.Println("[1] Generate Password")
     fmt.Println("[2] List Passwords")
     fmt.Println("[3] Delete Password")
+    fmt.Print("> ")
 }
 
 func main() {
-    fmt.Println("Hello world")
-
     start_msg_cli()
+    
+    var user_option int
+    fmt.Scanf("%v", &user_option)
+
+    switch user_option {
+    case 1:
+        fmt.Println("printing password") 
+    case 2:
+        fmt.Println("listing poasswords")
+    case 3:
+        fmt.Println("deleting password")
+    default:
+        fmt.Println("Not an option playboy")
+    }
 }
