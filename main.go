d4f52aa3b93726859f9e64c765be59d1e57b4da7
km <169460914+v49480nd-km@users.noreply.github.com>
2025-01-15 16:30:27 -0500

    updated gitignore and main.go because idk go yet so this ll be fun

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import ("fmt")
 
+func start_msg_cli() {
+    fmt.Println("PASSMAN")
+    fmt.Println("[1] Generate Password")
+    fmt.Println("[2] List Passwords")
+    fmt.Println("[3] Delete Password")
+}
+
 func main() {
     fmt.Println("Hello world")
+
+    start_msg_cli()
 }
